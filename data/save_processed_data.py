8339463378f5a667111a128150f8676a8212298f
Alex Wei <47674938+AlexWei21@users.noreply.github.com>
2023-12-04 14:30:32 -0500

    Update save_processed_data.py

--- data/save_processed_data.py
+++ data/save_processed_data.py
@@ -33,6 +33,13 @@ def save_processed_data(look_back_len, pred_len, with_meta_data, moving_average,
     #                                     data_smoothing=data_smoothing, data_frequency="Daily", meta_data_len=meta_data_len)
     
     # print(sars_data_list) # 5
+
+    # dengue_data_list = read_pandemic_ts(data_dir= "F:/Pandemic-Database/Processed_Data/Dengue_Fever/Dengue_AMRO_Country_Weekly_CumCases.csv", 
+    #                                     pandemic_name="Dengue", look_back_len=look_back_len, pred_len=pred_len, 
+    #                                     with_meta_data=with_meta_data, moving_average=moving_average,
+    #                                     data_smoothing=data_smoothing, data_frequency="Weekly", meta_data_len=meta_data_len)
+    
+    # print(dengue_data_list) # 2
     
 
 
