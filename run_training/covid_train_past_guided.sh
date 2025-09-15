1e37f640e7e17f4700f5bbe2325fb8279de24ce7
AlexWei21 <weiziming5115@gmail.com>
2025-09-15 10:49:00 -0400

    Update Covid Prediction with updated sampler

--- run_training/covid_train_past_guided.sh
+++ run_training/covid_train_past_guided.sh
@@ -8,17 +8,17 @@ dt=$(date +%m-%d-%H%M)
 #SBATCH -o run_logs/hostname_${dt}_%j.out
 #SBATCH -e run_logs/hostname_${dt}_%j.err
 
-train_len=56
+train_len=14
 
 # Determine batch_size based on train_len
 if [ "$train_len" -eq 56 ]; then
-    batch_size=1247
+    batch_size=1250
 elif [ "$train_len" -eq 42 ]; then
-    batch_size=1282
+    batch_size=1285
 elif [ "$train_len" -eq 28 ]; then
-    batch_size=1325 # Fill in the batch size for train_len=28
+    batch_size=1328
 elif [ "$train_len" -eq 14 ]; then
-    batch_size=1350  # Fill in the batch size for train_len=14
+    batch_size=1353  
 else
     echo "Error: Invalid train_len value. Supported values are 56, 42, 28, or 14."
     exit 1
